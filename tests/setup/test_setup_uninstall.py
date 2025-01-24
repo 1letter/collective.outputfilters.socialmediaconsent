45253129000ffb0eb717b7fe57c2261386142174
1letter <1letter@gmx.de>
2025-01-24 14:25:45 +0100

    add project initial

---
+++ tests/setup/test_setup_uninstall.py
@@ -0,0 +1,19 @@
+from collective.outputfilters.socialmediaconsent import PACKAGE_NAME
+
+import pytest
+
+
+class TestSetupUninstall:
+    @pytest.fixture(autouse=True)
+    def uninstalled(self, installer):
+        installer.uninstall_product(PACKAGE_NAME)
+
+    def test_addon_uninstalled(self, installer):
+        """Test if collective.outputfilters.socialmediaconsent is uninstalled."""
+        assert installer.is_product_installed(PACKAGE_NAME) is False
+
+    def test_browserlayer_not_registered(self, browser_layers):
+        """Test that IBrowserLayer is not registered."""
+        from collective.outputfilters.socialmediaconsent.interfaces import IBrowserLayer
+
+        assert IBrowserLayer not in browser_layers
